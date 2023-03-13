870107fcbc3b9db371ba43bda54c3e7bf70110f5
rodrigogk87 <rodrigogk87@gmail.com>
2023-03-13 15:54:43 -0300

    refactor coontract null ?

--- tsdapp/src/components/TicTacToe.tsx
+++ tsdapp/src/components/TicTacToe.tsx
@@ -46,11 +46,11 @@ export function TicTacToe(): ReactElement {
 
     useEffect(() => {
         if (contract)
-            getGameData();
+            getGameData(contract);
     }, [contract])
 
-    const getGameData = async () => {
-        const gameInfo = await contract?.getGameInfo(gameId);
+    const getGameData = async (contract: Contract) => {
+        const gameInfo = await contract.getGameInfo(gameId);
         setPlayers(gameInfo.players);
         setGameStarted(gameInfo.isStarted);
         const gameBoard = gameInfo.board.map((row: (string | null)[]) =>
@@ -62,37 +62,37 @@ export function TicTacToe(): ReactElement {
     }
 
     // create a new game
-    const handleCreateGame = async () => {
-        await contract?.createGame();
+    const handleCreateGame = async (contract: Contract) => {
+        await contract.createGame();
         setGameId((prevId: number) => prevId + 1);
     };
 
     // join an existing game
-    const handleJoinGame = async () => {
+    const handleJoinGame = async (contract: Contract) => {
         try {
-            await contract?.joinGame(gameId);
-            await getGameData();
+            await contract.joinGame(gameId);
+            await getGameData(contract);
         } catch (error) {
             console.log(error);
         }
     };
 
     // start the game
-    const handleStartGame = async () => {
+    const handleStartGame = async (contract: Contract) => {
         try {
-            await contract?.startGame(gameId);
-            await getGameData();
+            await contract.startGame(gameId);
+            await getGameData(contract);
         } catch (error) {
             console.log(error);
         }
     };
 
     // make a move on the board
-    const handleCellClick = async (row: number, col: number) => {
+    const handleCellClick = async (contract: Contract, row: number, col: number) => {
         try {
-            let tx = await contract?.play(gameId, row, col);
+            let tx = await contract.play(gameId, row, col);
             await tx.wait();
-            await getGameData();
+            await getGameData(contract);
         } catch (error) {
             console.log(error);
         }
@@ -104,56 +104,61 @@ export function TicTacToe(): ReactElement {
                 <h1>Tic Tac Toe Dapp</h1>
             </header>
             <main>
-                <div className="game-info">
-                    <h2>Game Information</h2>
-                    <p>Game ID: {gameId}</p>
-                    {gameStarted ? (
-                        <>
-                            <p>Players: {players.join(', ')}</p>
-                            <p>Current Player: {currentPlayer}</p>
-                            {tie || winner ? (
-                                <p>{tie ? "It's a tie!" : `Winner: ${winner}`}</p>
+                {contract &&
+                    <>
+                        <div className="game-info">
+                            <h2>Game Information</h2>
+                            <p>Game ID: {gameId}</p>
+                            {gameStarted ? (
+                                <>
+                                    <p>Players: {players.join(', ')}</p>
+                                    <p>Current Player: {currentPlayer}</p>
+                                    {tie || winner ? (
+                                        <p>{tie ? "It's a tie!" : `Winner: ${winner}`}</p>
+                                    ) : (
+                                        <p>Make your move, {currentPlayer}!</p>
+                                    )}
+                                </>
                             ) : (
-                                <p>Make your move, {currentPlayer}!</p>
+                                <>
+                                    <p>Waiting for playerx...</p>
+                                    <button onClick={() => handleJoinGame(contract)}>Join Game</button>
+                                </>
                             )}
-                        </>
-                    ) : (
-                        <>
-                            <p>Waiting for playerx...</p>
-                            <button onClick={handleJoinGame}>Join Game</button>
-                        </>
-                    )}
-                </div>
-                <div className="board">
-                    <Container fluid>
-                        {board.map((row, rowIndex) => (
-                            <Row key={rowIndex}>
-                                {row.map((cell, colIndex) => (
-                                    <Col xs={4}>
-                                        <div
-                                            className={`board-cell`}
-                                            onClick={() => handleCellClick(rowIndex, colIndex)}
-                                        >
-                                            {cell === players[0] ? 'X' : cell === players[1] ? 'O' : '_'}
-                                        </div>
-                                    </Col>
-                                ))}
-                            </Row>
-                        ))}
-                    </Container>
-
-
-                    {!gameStarted && (
-                        <div className="create-game">
-                            <button onClick={handleCreateGame}>Create Game</button>
                         </div>
-                    )}
-                </div>
-                {!gameStarted && !winner && (
-                    <div className="start-game">
-                        <button onClick={handleStartGame}>Start Game</button>
-                    </div>
-                )}
+                        <div className="board">
+                            <Container fluid>
+                                {board.map((row, rowIndex) => (
+                                    <Row key={rowIndex}>
+                                        {row.map((cell, colIndex) => (
+                                            <Col xs={4}>
+                                                <div
+                                                    className={`board-cell`}
+                                                    onClick={() => handleCellClick(contract, rowIndex, colIndex)}
+                                                >
+                                                    {cell === players[0] ? 'X' : cell === players[1] ? 'O' : '_'}
+                                                </div>
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                ))}
+                            </Container>
+
+
+                            {!gameStarted && (
+                                <div className="create-game">
+                                    <button onClick={() => handleCreateGame(contract)}>Create Game</button>
+                                </div>
+                            )}
+                        </div>
+                        {!gameStarted && !winner && (
+                            <div className="start-game">
+                                <button onClick={() => handleStartGame(contract)}>Start Game</button>
+                            </div>
+                        )}
+                    </>
+                }
+                {!contract && <h2> Loading... </h2>}
             </main>
             <footer>
                 <p>Powered by Ethereum and Hardhat.</p>

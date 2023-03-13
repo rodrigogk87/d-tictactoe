0ca61e01c3544ce86927fef99fcb5b455cbea16b
rodrigogk87 <rodrigogk87@gmail.com>
2023-03-13 15:41:54 -0300

    Update TicTacToe.tsx

--- tsdapp/src/components/TicTacToe.tsx
+++ tsdapp/src/components/TicTacToe.tsx
@@ -31,12 +31,6 @@ export function TicTacToe(): ReactElement {
         }
 
         setSigner(library.getSigner());
-        const contract = new ethers.Contract(
-            "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            TicTacToeContract.abi,
-            library.getSigner()
-        );
-        setContract(contract);
     }, [library]);
 
     useEffect((): void => {
